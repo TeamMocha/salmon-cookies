3b5b20394818cd19300ba79b050a97017a841b98
Jeremy Penning <4085868+pixeljava@users.noreply.github.com>
2021-02-05 04:07:57 -0800

    Refactoring to move tabs up finally worked. Did a little work on the site, just needs the sales table working, but that's almost done too.

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,2 +1,51 @@
+<script>
+  import { url, isActive } from "@roxi/routify";
+  import { TabsTransition } from "@roxi/routify/decorators";
+  import TopHeader from "./_components/TopHeader.svelte";
+  import BottomNav from "./_components/BottomNav.svelte";
+
+  const headerHeight = '90px';
+  const footerNavHeight = '56px';
+
+  const _urls = [
+    ["./index", "Home", "#7fc5bb"],
+    ["./stores", "Stores", "#0bf5cc"],
+    ["./sales", "Sales Info", "#88f0d0"]
+  ];
+  $: urls = _urls.map(([path, name, color]) => ({
+    name,
+    href: $url(path),
+    color,
+    active: !!$isActive(path),
+  }));
+</script>
+
 <!-- routify:options preload="proximity" -->
-<slot />
+
+<style>
+  :global(body) {
+    padding: 0;
+  }
+  * :global(.inset) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  main.inset {
+    overflow: hidden;
+  }
+
+  /* * :global(*) {
+    text-align: center;
+  } */
+</style>
+
+<div style="height: 100%">
+  <TopHeader {urls} height="64px" />
+  <main class="inset" style="top: { headerHeight }; bottom: { footerNavHeight };">    
+    <slot decorator={TabsTransition} />    
+  </main>
+  <BottomNav {urls} height={ footerNavHeight } />
+</div>
---
+++ src/pages/stores/[id]/index.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let id;
+  import { url } from "@roxi/routify";
+  import { allStores } from '../../_data.js'
+  let store = allStores[id];
+</script>
+
+<style>
+  a {
+    font-size: 1.5em;
+    padding: 12px 24px;
+  }
+</style>
+
+<div class="card" style="width: 512px; height: auto;">
+  <img alt={store.name} src={`https://source.unsplash.com/${store.img_tag}/512x200`} />
+  <h1>{store.name}</h1>
+  <h2>Visit at: {store.address}</h2>
+  <h2>Call Us: {store.phone}</h2>
+</div>
+
+<br />
+
+<a href={$url('/')}>Go home</a>
+<a href={$url('../')}>Go back</a>
